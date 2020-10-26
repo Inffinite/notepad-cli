322022f59ae2f7679ed40ec174f102984214d797
inffinite <louislaizer4@gmail.com>
2020-10-26 19:03:56 +0300

    Added notes support
    Separated it from tasks

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 const tasks = require('../lib/tasks.js')
+const notes = require('../lib/notes.js')
 tasks.initializeDb()
+notes.initializeDb()
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('notes')
 
@@ -9,32 +11,48 @@ let args = process.argv.slice(2)
 
 setTimeout(() => {
     switch (args[0]) {
-        case '--add-task' || '-a':
-            tasks.addNote(args[1])
+        case '--add-task':
+            tasks.addTask(args[1])
+            break;
+
+        case '--add-note':
+            notes.addNote(args[1])
             break;
 
         case '--all-tasks':
-            tasks.getAllNotes()
+            tasks.getAllTasks()
+            break;
+
+        case '--all-notes':
+            notes.getAllNotes()
             break;
 
         case '--done-task':
             tasks.markCompleted(args[1])
             break;
 
-        case '--complete-tasks' || '-c':
-            tasks.getCompleteNotes()
+        case '--complete-tasks':
+            tasks.getCompleteTasks()
             break;
 
-        case '--incomplete-tasks' || '-i':
-            tasks.getIncompleteNotes()
+        case '--incomplete-tasks':
+            tasks.getIncompleteTasks()
             break;
 
-        case '--purge-tasks' || '-p':
-            tasks.purgeAllNotes()
+        case '--purge-tasks':
+            tasks.purgeAllTasks()
             break;
 
-        case '--delete-task' || '-d':
-            tasks.deleteNote(args[1])
+        case '--purge-notes':
+            notes.purgeAllNotes()
+            break;
+
+        case '--delete-task':
+            tasks.deleteTask(args[1])
+            break;
+
+        case '--delete-note':
+            notes.deleteNote(args[1])
             break;
 
         default:
